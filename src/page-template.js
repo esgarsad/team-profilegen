e4080bef1de95c1fe8a1844cf1e798cc51c2e287
esgar <navaedgar36@yahoo.com.mx>
2022-04-17 13:17:08 -0500

    css added

--- src/page-template.js
+++ src/page-template.js
@@ -36,7 +36,7 @@ const generateEmployeeCards = employeesArray => {
       .join('')}
       ${employeesArray
       .filter(({ title }) => title === 'Engineer')
-      .map(({ name, id, email,title, github }) => {
+      .map(({ name, id, email,title, gitHub }) => {
         return `
           <div class='col-3'>
             <div class='card'>
@@ -107,7 +107,7 @@ module.exports = templateData => {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Organization Chart</title>
+    <title>Team-gen</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
         integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
